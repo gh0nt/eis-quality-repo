7c5f5b076b398ad37ddf48344c1e617949dfaa3b
Ghont <giohan.puentes.hernandez@unillanos.edu.co>
2025-01-07 22:38:47 -0500

    Update test1.py

--- TestingFiles/test1.py
+++ TestingFiles/test1.py
@@ -1,15 +1,15 @@
 from funciones import saludar
 import unittest
 
-class TestCalculaMedia(unittest.TestCase):
+class TestSaludar(unittest.TestCase):
     
     def test_1(self):
-        resultado = saludar("Daniel")
-        self.assertEqual(resultado, "Hola, Daniel")
+        resultado = saludar("Marcelo")
+        self.assertEqual(resultado, "Hola, Marcelo")
 
     def test_2(self):
         resultado = saludar("Juan")
-        self.assertEqual(resultado, "Hola Juan")
+        self.assertEqual(resultado, "Hola, Juan")
 
 if __name__ == '__main__':
     unittest.main()
