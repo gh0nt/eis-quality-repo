0d09918bbb6752ecfa2867cecbc8c53304984255
Ghont <giohan.puentes.hernandez@unillanos.edu.co>
2025-01-07 23:07:37 -0500

    Update test1.py for errors JENKIN TEST FAILURE

--- TestingFiles/test1.py
+++ TestingFiles/test1.py
@@ -4,11 +4,11 @@ import unittest
 class TestSaludar(unittest.TestCase):
     
     def test_1(self):
-        resultado = saludar("Marcelo")
+        resultado = saludar("Gabriel")
         self.assertEqual(resultado, "Hola, Marcelo")
 
     def test_2(self):
-        resultado = saludar("Juan")
+        resultado = saludar("Ghost")
         self.assertEqual(resultado, "Hola, Juan")
 
 if __name__ == '__main__':
